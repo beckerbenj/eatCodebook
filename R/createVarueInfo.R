d2446e830eb51428ba91f70c2bdf9bf8ceb5ded5
Benjamin Becker <benj.becker@web.de>
2021-08-23 16:49:00 +0200

    better defaults createVarueInfo

--- R/createVarueInfo.R
+++ R/createVarueInfo.R
@@ -5,6 +5,7 @@
 #' Create variable information (background model, which variables in the data set, ...) template.
 #'
 #'@param GADSdat \code{GADSdat} object.
+#'@param inputForDescriptives Input for descriptives calculation.
 #'@param encodingList tbd.
 #'@param makeStructure Should an automatic structuring of variables be created_
 #'
@@ -14,26 +15,42 @@
 #'#tbd
 #'
 #'@export
-createVarueInfo <- function(GADSdat, encodingList = NULL, makeStructure = TRUE){
+createVarueInfo <- function(GADSdat, inputForDescriptives, encodingList = NULL, makeStructure = TRUE){
   UseMethod("createVarueInfo")
 }
 #'@export
-createVarueInfo.GADSdat <- function(GADSdat, encodingList = NULL, makeStructure = TRUE){
+createVarueInfo.GADSdat <- function(GADSdat, inputForDescriptives, encodingList = NULL, makeStructure = TRUE){
   var_labs <- unique(eatGADS::extractMeta(GADSdat)[, c("varName", "varLabel")])
-  n <- ncol(GADSdat$dat)
 
+  var_labs2 <- var_labs
+  inputed_info <- inputForDescriptives[inputForDescriptives$imp == TRUE, ]
+  pooled_variables <- unique(inputed_info[, "group"])
+  for(i in pooled_variables) {
+    #browser()
+    single_inputed_info <- inputed_info[inputed_info$group == i, ]
+    first_entry <- single_inputed_info[1, "varName"]
+    newRow <- var_labs2[var_labs2$varName == first_entry, ]
+    newRow[, "varName"] <- i
+
+    var_labs2 <- insertRow(var_labs2, newRow = newRow, index = which(var_labs2$varName == first_entry))
+  }
+
+  # insert pooled variables here? with modified variable label
+  # defaults in.DS.und.SH (ja/sh/ds)
+
+  n <- nrow(var_labs2)
   g <- rep("-" , n)
   if(makeStructure) g <- 1:n
 
   variableninfo <- data.frame(
-    "Var.Name" = var_labs$varName,
+    "Var.Name" = var_labs2$varName,
     "in.DS.und.SH" = rep("ja" , n),
     "Layout" = rep("-" , n),
-    "LabelSH" = var_labs$varLabel,
+    "LabelSH" = var_labs2$varLabel,
     "Anmerkung.Var" = rep("-" , n),
     "Gliederung" = g,
     "Reihenfolge" = rep("-" , n),
-    "Titel" = var_labs$varLabel,
+    "Titel" = var_labs2$varLabel,
     "rekodiert" = rep("nein" , n),
     "QuelleSH" = rep("-" , n),
     "Instruktionen" = rep("-" , n),
@@ -43,6 +60,9 @@ createVarueInfo.GADSdat <- function(GADSdat, encodingList = NULL, makeStructure 
     "intern.extern" = rep("-" , n) ,
     "Seitenumbruch.im.Inhaltsverzeichnis" = rep("nein" , n) ,
     stringsAsFactors=FALSE)
+
+  variableninfo[, "in.DS.und.SH"] <- ifelse(variableninfo[, "Var.Name"] %in% pooled_variables, yes = "sh", no = variableninfo[, "in.DS.und.SH"])
+  variableninfo[, "in.DS.und.SH"] <- ifelse(variableninfo[, "Var.Name"] %in% inputed_info$varName, yes = "ds", no = variableninfo[, "in.DS.und.SH"])
 
   if(!is.null(encodingList)) {
     for( i in 1:length(encodingList$input)){
@@ -54,9 +74,16 @@ createVarueInfo.GADSdat <- function(GADSdat, encodingList = NULL, makeStructure 
   variableninfo
 }
 #'@export
-createVarueInfo.list <- function(GADSdat, encodingList = NULL, makeStructure = TRUE){
-  lapply(GADSdat, function(x) {
-    createVarueInfo(x, encodingList = encodingList, makeStructure = makeStructure)
-  })
+createVarueInfo.list <- function(GADSdat, inputForDescriptives, encodingList = NULL, makeStructure = TRUE){
+  Map(function(single_GADSdat, single_input) {
+    createVarueInfo(single_GADSdat, single_input, encodingList = encodingList, makeStructure = makeStructure)
+  }, single_GADSdat = GADSdat, single_input = inputForDescriptives)
+}
+
+
+insertRow <- function(df, newRow, index) {
+  newDF <- rbind(df, newRow)
+  newDF <- newDF[order(c(1:(nrow(newDF) - 1), index - 0.5)), ]
+  newDF
 }
 
--- tests/testthat/test-createVarueInfo.R
+++ tests/testthat/test-createVarueInfo.R
@@ -2,19 +2,28 @@
 #dfSAV <- eatGADS::import_spss("tests/testthat/helper_spss.sav")
 dfSAV <- eatGADS::import_spss("helper_spss.sav")
 
+#gads <- readRDS("tests/testthat/helper_scaleDF.RDS")
+gads <- readRDS("helper_scaleDF.RDS")
+
 test_that("with pisa", {
-  out <- createVarueInfo(eatGADS::pisa)
-  expect_equal(out$Var.Name, eatGADS::namesGADS(eatGADS::pisa))
+  suppressMessages(input4descr <- createInputForDescriptives(eatGADS::pisa, impExpr = "Plausible Value"))
+  out <- createVarueInfo(eatGADS::pisa, input4descr)
+  expect_true(all(eatGADS::namesGADS(eatGADS::pisa) %in% out$Var.Name))
+  expect_true(all(c("ma_pooled", "rea_pooled", "sci_pooled") %in% out$Var.Name))
   expect_equal(out$in.DS.und.SH[1], "ja")
+  expect_equal(sum(out$in.DS.und.SH == "sh"), 3)
+  expect_equal(sum(out$in.DS.und.SH == "ds"), 3 * 5)
   expect_equal(out$LabelSH[1], "Student-ID")
   expect_equal(out$LabelSH, out$Titel)
 })
 
 test_that("with list", {
   l1 <- list(pisa = eatGADS::pisa, other = dfSAV)
-  out <- createVarueInfo(l1)
+  suppressMessages(inputList <- createInputForDescriptives(l1, impExpr = "Plausible Value"))
+  out <- createVarueInfo(l1, inputList)
   expect_equal(names(out), c("pisa", "other"))
-  expect_equal(out[[1]]$Var.Name, eatGADS::namesGADS(eatGADS::pisa))
+  expect_true(all(eatGADS::namesGADS(eatGADS::pisa) %in% out[[1]]$Var.Name))
+  expect_true(all(c("ma_pooled", "rea_pooled", "sci_pooled") %in% out[[1]]$Var.Name))
   expect_equal(out[[1]]$LabelSH, out[[1]]$Titel)
   expect_equal(out[[2]]$Var.Name, eatGADS::namesGADS(dfSAV))
 })
