83afcbcc3a9a5fb3363a5745dccd34e78e258be2
Ben <benj.becker@web.de>
2021-08-25 12:32:05 +0200

    calculateDescriptives test in own file

--- tests/testthat/test-descriptives_helper.R
+++ tests/testthat/test-descriptives_helper.R
@@ -127,41 +127,6 @@ test_that("descriptives metric", {
 })
 
 
-### probleme
-
-test_that("descriptives dummy", {
-  df <- data.frame(id = 1:5, v1 = c(3, 3, 1, 2, -99))
-  value_table <- data.frame(value = c(1, 2, 3, -98, -99),
-                            missings = c("valid", "valid", "valid", "miss", "miss"),
-                            stringsAsFactors = FALSE)
-  out <- suppressWarnings(kennwerte.ordinal.skala(df[,"v1"], value_table))
-})
-
-
-### tests fuer die gesamte createInputForDescriptives- und calculateDescriptives-Funktion
-
-### Achtung: wenn alle Probleme behoben sind, hier "suppressWarnings" entfernen und die
-### Warnungen explizit abfangen (die soll es ja geben)
-
-#load("tests/testthat/helper_varinfo_vorlage.rda")
-load("helper_varinfo_vorlage.rda")
-test_that("descriptives scale", {
-  # create GADSdat
-    file <- system.file("extdata", "example1.sav", package = "eatCodebook")
-    gd   <- eatGADS::import_spss(file)                                          ### inputliste erzeugen
-    vari <- suppressWarnings(createInputForDescriptives(gd, varNameSeparatorImp = NULL, impExpr = "plausible value", scaleExpr = "Skalenwert", verbose = FALSE))
-### hier gibt es eine wichtige warnmeldung, die beruecksichtigt werden muss,
-### dass das skalenniveau der items, die zu einer skala gehoeren, einheitlich sein muss
-### korrektur von hand
-    vari[grep("skala1item", vari[,"varName"]),"scale"] <- "ordinal"
-    expect_equal(dim(vari), c(22,7))                                            ### data.frame mit 22 Zeilen, 7 Spalten
-    tab  <- table(vari[,"type"])
-    expect_equal(names(tab), c("scale", "variable"))                            ### beide eintraege sollen vorkommen
-    expect_equal(as.vector(tab), c(1,21))                                       ### scale nur einmal, variable 21-mal
-    vari[which(vari[,"varName"] == "skalenwert_fake"),"type"] <- "scale"        ### ein eintrag in der varinfo muss jetzt haendisch geaendert werden (das geschieht spaeter fuer das
-    res  <- suppressMessages(calculateDescriptives(gd, vari))                   ### tatsaechliche Skalenhandbuch bei Bedarf in Excel)
-})
-
 
 
 
