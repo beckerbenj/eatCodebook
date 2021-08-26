3e3fd3753fa78b466274329532751283e04dbaa9
Ben <benj.becker@web.de>
2021-08-26 12:07:04 +0200

    fix some tests

--- tests/testthat/test-createRegister.R
+++ tests/testthat/test-createRegister.R
@@ -1,17 +1,18 @@
 
-#varInfo_pisa <- readRDS("tests/testthat/helper_varinfo_pisa.RDS")
-varInfo_pisa <- readRDS("helper_varinfo_pisa.RDS")
+#input_pisa <- readRDS("tests/testthat/helper_inputForDescriptives_pisa.RDS")
+input_pisa <- readRDS("helper_inputForDescriptives_pisa.RDS")
 
-#load("tests/testthat/helper_varinfo.rda")
-load("helper_varinfo.rda")
+#input_clean <- readRDS("tests/testthat/helper_inputForDescriptives_clean.RDS")
+input_clean <- readRDS("helper_inputForDescriptives_clean.RDS")
+
 
 test_that("for standard data.frame", {
-  out <- createRegister(varinfo, keywordList = c("KW1", "KW2"))
+  out <- createRegister(input_clean, keywordList = c("KW1", "KW2"))
   expect_equal(names(out), c("Nr", "varName", "KW1", "KW2"))
-  expect_equal(out$Nr, seq(length(unique(varinfo$group))))
-  expect_equal(out$varName, unique(varinfo$group))
-  expect_equal(out$KW1, rep(NA, 7))
-  expect_equal(out$KW2, rep(NA, 7))
+  expect_equal(out$Nr, seq(length(unique(input_clean$group))))
+  expect_equal(out$varName, unique(input_clean$group))
+  expect_equal(out$KW1, rep(NA, 22))
+  expect_equal(out$KW2, rep(NA, 22))
 })
 
 
--- tests/testthat/test-createStructure.R
+++ tests/testthat/test-createStructure.R
@@ -1,14 +1,14 @@
 
-#varInfo <- readRDS("tests/testthat/helper_varinfo_pisa.RDS")
-varInfo <- readRDS("helper_varinfo_pisa.RDS")
+#input <- readRDS("tests/testthat/helper_inputForDescriptives_clean.RDS")
+input <- readRDS("helper_inputForDescriptives_clean.RDS")
 
 test_that("for data.frame", {
-  out <- createStructure(varInfo)
+  out <- createStructure(input)
   expect_equal(names(out), c("Titel", "Ebene"))
 })
 
 test_that("for list", {
-  l <- list(dat1 = varInfo, dat2 = varInfo)
+  l <- list(dat1 = input, dat2 = input)
   out <- createStructure(l)
   expect_equal(names(out), c("dat1", "dat2"))
   expect_equal(names(out[[1]]), c("Titel", "Ebene"))
