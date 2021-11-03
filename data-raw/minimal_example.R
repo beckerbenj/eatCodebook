3244fd3ec18d62620e3b8b8380450d83c069441d
Benjamin Becker <benj.becker@web.de>
2021-11-03 15:22:25 +0100

    minimal example as far as code goes

--- data-raw/minimal_example.R
+++ data-raw/minimal_example.R
@@ -12,41 +12,73 @@ inputForDescr[grep("skala1item", inputForDescr[,"varName"]),"scale"] <- "ordinal
 inputForDescr[which(inputForDescr[,"varName"] == "skalenwert_fake"),"type"] <- "scale"        ### ein eintrag in der varinfo muss jetzt haendisch geaendert werden (das geschieht spaeter fuer das
 descr  <- calculateDescriptives(gd, inputForDescr)
 
-## ## prepare everything else
+## Missings/Line Breaks
 miss <- createMissings(gd)
 eatAnalysis::write_xlsx(miss, "inst/extdata/example_miss.xlsx", row.names = FALSE)
 miss_final <- getMissings("inst/extdata/example_miss.xlsx")
 
-
+## Varinfo
 varInfo <- createVarInfo(gd, inputForDescriptives = inputForDescr)
 varInfo[3, "QuelleSH"] <- "Mueller (2019)"
-varInfo[c(2, 11:14), "Hintergrundmodell"] <- "ja"
+varInfo[c(2, 3, 8), "Hintergrundmodell"] <- "ja"
 eatAnalysis::write_xlsx(varInfo, "inst/extdata/example_varInfo.xlsx", row.names = FALSE)
 varInfo_final <- getVarInfo("inst/extdata/example_varInfo.xlsx")
 
+## Structure
 struc <- createStructure(varInfo_final)
 eatAnalysis::write_xlsx(struc, "inst/extdata/example_struc.xlsx", row.names = FALSE)
 struc_final <- getStructure("inst/extdata/example_struc.xlsx")
 
+## Scale Infos
 scaleInfo <- createScaleInfo(inputForDescr)
+eatAnalysis::write_xlsx(scaleInfo, "inst/extdata/example_scaleInfo.xlsx", row.names = FALSE)
+scaleInfo_final <- getScaleInfo("inst/extdata/example_scaleInfo.xlsx")
 
+## Register
 register <- createRegister(inputForDescr, keywordList = c("kw1", "kw2"))
+eatAnalysis::write_xlsx(scaleInfo, "inst/extdata/example_scaleInfo.xlsx", row.names = FALSE)
+scaleInfo_final <- getScaleInfo("inst/extdata/example_scaleInfo.xlsx")
 
-
+## Abbrevtiation list
 abbrList <- createAbbrList()
 abbrList[[1]][1, ] <- c("MW", "Mittelwert")
 abbrList[[2]][1, ] <- c("M", "Mittelwert")
 eatAnalysis::write_xlsx(abbrList, "inst/extdata/example_abbrList.xlsx", row.names = FALSE)
 
+## Literature
 litInfo <- createLitInfo(varInfo)
+litInfo[, 3] <- "ja"
+litInfo[, 2] <- "Mueller, M. (2020). Titel."
+eatAnalysis::write_xlsx(litInfo, "inst/extdata/example_litInfo.xlsx", row.names = FALSE)
+litInfo_final <- getLitInfo("inst/extdata/example_litInfo.xlsx")
 
-## make-steps
+
+## Make-steps
+# --------------------------------------------------
 cover <- makeCover(maintitle = "Study of Achievement", subtitle = "Codebook of Study of Achievement",
                    authors = "Some Person", addAuthors = "With the help of some other persons",
                    schriftenreihe = "Book 9 of Studies of Achievement")
 
 abbr <- makeAbbrList("inst/extdata/example_abbrList.xlsx")
 
-lit <- makeLit(litInfo)
+lit <- makeLit(litInfo_final)
 
-hint <- makeBGM
+hint <- makeBGM(varInfo_final)
+
+
+## Sonstiges Zeug von Felix
+# --------------------------------------------------
+# SH-Variablen
+variablen.all <- varInfo_final[varInfo_final %in% c("ja", "sh"), "Var.Name"]
+
+# ID-Variablen
+id <- c("id")
+
+
+## Ueberlegungen
+# --------------------------------------------------
+# getExcel umschreiben, sodass im Zweifelsfall doch immer eine Liste (bei Input df -> Laenge 1 rauskommst?)
+
+
+
+
