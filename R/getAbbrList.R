209ba72647f0615069a20a850e7effaf62ac9c7f
Benjamin Becker <benj.becker@web.de>
2021-11-05 16:42:16 +0100

    deprecate getAbbrList

--- R/getAbbrList.R
+++ R/getAbbrList.R
@@ -14,5 +14,6 @@
 #'
 #'@export
 getAbbrList <- function(filePath){
+  stop("'getAbbrList' is deprecated, use 'makeAbbrList' instead.")
   getExcel(filePath)
 }
--- tests/testthat/test-getAbbrList.R
+++ tests/testthat/test-getAbbrList.R
@@ -4,8 +4,9 @@
 #eatAnalysis::write_xlsx(abbrList, filePath = "tests/testthat/helper_abbrList")
 
 test_that("with defaults", {
-  out <- getAbbrList("helper_abbrList.xlsx")
-  expect_equal(length(out), 2)
-  expect_equal(names(out), c("Statistische Formelzeichen", "Akronyme"))
-  expect_equal(dim(out[[2]]), c(1, 2))
+  expect_error(getAbbrList("helper_abbrList.xlsx"), "'getAbbrList' is deprecated, use 'makeAbbrList' instead.")
+  # out <- getAbbrList("helper_abbrList.xlsx")
+  # expect_equal(length(out), 2)
+  # expect_equal(names(out), c("Statistische Formelzeichen", "Akronyme"))
+  # expect_equal(dim(out[[2]]), c(1, 2))
 })
