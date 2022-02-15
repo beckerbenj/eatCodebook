097604d024fa6139a8c74b5f00bd5f65ddc98839
Benjamin Becker <benj.becker@web.de>
2022-02-15 18:46:33 +0100

    minimal example progress

--- data-raw/minimal_example.R
+++ data-raw/minimal_example.R
@@ -13,7 +13,7 @@ descr  <- calculateDescriptives(gd, inputForDescr)
 
 ## Missings/Line Breaks
 miss <- createMissings(gd, inputForDescriptives = inputForDescr)
-eatAnalysis::write_xlsx(miss, "inst/extdata/example_miss.xlsx", row.names = FALSE)
+#eatAnalysis::write_xlsx(miss, "inst/extdata/example_miss.xlsx", row.names = FALSE)
 miss_final <- getMissings("inst/extdata/example_miss.xlsx")
 
 ## Varinfo
@@ -26,7 +26,7 @@ varInfo[c(1), "Titel"] <- "Schueler-ID"
 varInfo[, "Unterteilung.im.Skalenhandbuch"] <- c(rep("BG", 4), rep("Scale", 4), rep("PVs", 12))
 varInfo[, "Gliederung"] <- c(rep("1.1", 4), rep("1.2", 4), rep("2.1", 12))
 varInfo[c(9, 15), "Titel"] <- c("Plausible Value", "categorical plausible value")
-eatAnalysis::write_xlsx(varInfo, "inst/extdata/example_varInfo.xlsx", row.names = FALSE)
+#eatAnalysis::write_xlsx(varInfo, "inst/extdata/example_varInfo.xlsx", row.names = FALSE)
 varInfo_final <- getVarInfo("inst/extdata/example_varInfo.xlsx")
 varInfo_final2 <- inferLayout(varInfo_final, GADSdat = gd, inputForDescriptives = inputForDescr)
 
@@ -34,12 +34,12 @@ varInfo_final2 <- inferLayout(varInfo_final, GADSdat = gd, inputForDescriptives 
 struc <- createStructure(varInfo_final)
 struc[c(1, 4), "Titel"] <- c("Background", "Competences")
 # Hier Oberkapitel einfuegen
-eatAnalysis::write_xlsx(struc, "inst/extdata/example_struc.xlsx", row.names = FALSE)
+#eatAnalysis::write_xlsx(struc, "inst/extdata/example_struc.xlsx", row.names = FALSE)
 struc_final <- getStructure("inst/extdata/example_struc.xlsx")
 
 ## Scale Infos
 scaleInfo <- createScaleInfo(inputForDescr)
-eatAnalysis::write_xlsx(scaleInfo, "inst/extdata/example_scaleInfo.xlsx", row.names = FALSE)
+#eatAnalysis::write_xlsx(scaleInfo, "inst/extdata/example_scaleInfo.xlsx", row.names = FALSE)
 scaleInfo_final <- getScaleInfo("inst/extdata/example_scaleInfo.xlsx")
 # workaround, should be done automatically, but currently problem with non list inputs for createScaleInfo
 scaleInfo_final$Quelle <- "dat"
@@ -48,20 +48,20 @@ scaleInfo_final$Quelle <- "dat"
 register <- createRegister(inputForDescr, keywordList = c("kw1", "kw2"))
 register[c(1, 3, 4), "kw1"] <- "x"
 register[c(7), "kw2"] <- "x"
-eatAnalysis::write_xlsx(register, "inst/extdata/example_register.xlsx", row.names = FALSE)
+#eatAnalysis::write_xlsx(list(dat = register), "inst/extdata/example_register.xlsx", row.names = FALSE)
 register_final <- getRegister("inst/extdata/example_register.xlsx")
 
 ## Abbrevtiation list
 abbrList <- createAbbrList()
 abbrList[[1]][1, ] <- c("MW", "Mittelwert")
 abbrList[[2]][1, ] <- c("M", "Mittelwert")
-eatAnalysis::write_xlsx(abbrList, "inst/extdata/example_abbrList.xlsx", row.names = FALSE)
+#eatAnalysis::write_xlsx(abbrList, "inst/extdata/example_abbrList.xlsx", row.names = FALSE)
 
 ## Literature
 litInfo <- createLitInfo(varInfo)
 litInfo[, 3] <- "ja"
 litInfo[, 2] <- "Mueller, M. (2020). Titel."
-eatAnalysis::write_xlsx(litInfo, "inst/extdata/example_litInfo.xlsx", row.names = FALSE)
+#eatAnalysis::write_xlsx(litInfo, "inst/extdata/example_litInfo.xlsx", row.names = FALSE)
 litInfo_final <- getLitInfo("inst/extdata/example_litInfo.xlsx")
 # vlt. getLitInfo ueberarbeiten, sodass in_Litverzeichnis obsolet wird (also doppelte Langangabe rausnehmen)
 
@@ -71,7 +71,7 @@ meta[1, "Title"] <- "Codebook Test"
 meta[1, "Author"] <- "Anna Muster"
 meta[1, "Keywords"] <- "lsa, education"
 meta[1, "Subject"] <- "test"
-eatAnalysis::write_xlsx(meta, "inst/extdata/example_meta.xlsx", row.names = FALSE)
+#eatAnalysis::write_xlsx(meta, "inst/extdata/example_meta.xlsx", row.names = FALSE)
 
 
 ## Make-steps
@@ -114,13 +114,20 @@ names(struc_final2) <- "dat"
 #str(descr$skala1[[2]])
 descr$skala1[[2]] <- as.data.frame(descr$skala1[[2]])
 
-codebook(varue.info = list(dat = varInfo_final2), varue.missings = list(dat = miss_final), varue.gliederung = struc_final2,
+latex_skript <- codebook(varue.info = list(dat = varInfo_final2), varue.missings = list(dat = miss_final), varue.gliederung = struc_final2,
          skalen.info = scaleInfo_final,
-         varue.reg = list(dat = register_final), make.reg = NULL, Gesamtdatensatz = list(dat = eatGADS::extractData(gd)),
+         varue.reg = register_final, make.reg = NULL, Gesamtdatensatz = list(dat = eatGADS::extractData(gd)),
          Kennwertedatensatz = list(dat = descr),
          variablen = list(dat = variablen.all),
          id = id, fbshort = "dat", fblong = "dat", deckblatt = "", intro = "", literatur = lit, abkuerzverz = abbr, hintmod = hint,
          lastpage = "")
+
+#### SKRIPTE SCHREIBEN ####
+write.table(latex_skript , file = "other_code/minimal_example/minimal_example.tex" , fileEncoding="UTF-8" ,
+            col.names=FALSE , row.names=FALSE , quote = FALSE )
+write.table(meta_final , file = "other_code/minimal_example/minimal_example_meta.xmpdata", fileEncoding="UTF-8" ,
+            col.names=FALSE , row.names=FALSE , quote = FALSE )
+
 
 
 
@@ -131,4 +138,10 @@ codebook(varue.info = list(dat = varInfo_final2), varue.missings = list(dat = mi
 
 
 #load("q:/BT2016/BT/02_Organisation/81_StuMi_Arbeitsordner/Skalenhandbuch/03_Kennwerte/Kennwerte_sfb.rdata")
+Kennwertedatensatz$stufe_deu_hoeren_gepoolt
+Kennwertedatensatz$stufe_deu_hoeren_gepoolt
 
+# mit JB sprechen, ohjeohje!
+load("q:/BT2018/BT/90_Skalenhandbuch/07_SH_Erstellung/Minimalbeispiel/Erstellung_alte_syntax/03_Kennwerte/Kennwerte_minimal.rdata")
+Kennwertedatensatz$pvkat_1
+#Kennwertedatensatz$
