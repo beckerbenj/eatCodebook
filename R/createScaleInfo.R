7991be67113b3a28346c6143b030d63a1f5df987
Benjamin Becker <benj.becker@web.de>
2023-12-05 13:14:39 +0100

    Implement fake scales into createScaleInfo

--- R/createScaleInfo.R
+++ R/createScaleInfo.R
@@ -2,7 +2,7 @@
 #############################################################################
 #' Create scale information.
 #'
-#' Create information on scale aggregation.
+#' Create information on scale aggregation. Scales, 'fake scales', and imputed variables are listed.
 #'
 #'@param inputForDescriptives \code{inputForDescriptives} object as created by the \code{createInputForDescriptives} function
 #         source character string of length 1: source information of the data, for example \code{"sfb"}, \code{"lfb"}, etc.
@@ -28,8 +28,9 @@ createScaleInfo.data.frame <- function(inputForDescriptives){
                           Anzahl_valider_Werte = character(),	Items_der_Skala = character(), stringsAsFactors = FALSE)
 
   ## Scales
+  #browser()
   inputForDescriptives <- check_inputForDescriptives(inputForDescriptives)
-  scales_inputForDescriptives <- inputForDescriptives[which(inputForDescriptives$type == "scale"), ]
+  scales_inputForDescriptives <- inputForDescriptives[which(inputForDescriptives$type %in% c("scale", "fake_item")), ]
   scales <- scales_inputForDescriptives[!duplicated(scales_inputForDescriptives$group), "group"]
 
   ## Inputed
@@ -50,7 +51,8 @@ createScaleInfo.data.frame <- function(inputForDescriptives){
       next
     }
     scaleInfo[i, "varName"] <- unique(single_inputForDescriptives$group)
-    scaleInfo[i, "Items_der_Skala"] <- paste(single_inputForDescriptives[single_inputForDescriptives$type == "variable", "varName"], collapse = ",")
+    items <- single_inputForDescriptives[single_inputForDescriptives$type %in% c("item", "fake_item"), "varName"]
+    scaleInfo[i, "Items_der_Skala"] <- paste(items, collapse = ",")
     scaleInfo[i, "Anzahl_valider_Werte"] <- "-"
     scaleInfo[i, "Quelle"] <- source
   }
@@ -63,7 +65,8 @@ createScaleInfo.list <- function(inputForDescriptives){
     createScaleInfo(x)
   })
 
-  scaleInfo <- eatTools::do_call_rbind_withName(scaleInfo_list, colName = "Quelle2")[, c("varName", "Quelle2", "Anzahl_valider_Werte",
+  scaleInfo <- eatTools::do_call_rbind_withName(scaleInfo_list, colName = "Quelle2")[, c("varName", "Quelle2",
+                                                                                         "Anzahl_valider_Werte",
                                                                                          "Items_der_Skala")]
   names(scaleInfo)[2] <- "Quelle"
   scaleInfo
--- tests/testthat/test-createScaleInfo.R
+++ tests/testthat/test-createScaleInfo.R
@@ -1,20 +1,24 @@
 
-#input_pisa <- readRDS("tests/testthat/helper_inputForDescriptives_pisa.RDS")
-input_pisa <- readRDS("helper_inputForDescriptives_pisa.RDS")
-#input_clean <- readRDS("tests/testthat/helper_inputForDescriptives_clean.RDS")
-input_clean <- readRDS("helper_inputForDescriptives_clean.RDS")
-#input_imputedScale <- readRDS("tests/testthat/helper_inputedForDescriptives_imputedScale.RDS")
-input_imputedScale <- readRDS("helper_inputedForDescriptives_imputedScale.RDS")
+input_pisa <- readRDS(test_path("helper_inputForDescriptives_pisa.RDS"))
+input_clean <- readRDS(test_path("helper_inputForDescriptives_clean.RDS"))
+input_clean2 <- readRDS(test_path("helper_inputForDescriptives_clean2.RDS"))
+input_imputedScale <- readRDS(test_path("helper_inputedForDescriptives_imputedScale.RDS"))
 
 
 test_that("single scale", {
   out <- createScaleInfo(input_clean)
-  expect_equal(nrow(out), 4)
-  expect_equal(out$varName, c("skala1", "skalenwert_fake", "pv_pooled", "pv_kat_pooled"))
+  expect_equal(nrow(out), 3)
+  expect_equal(out$varName, c("skala1", "pv_pooled", "pv_kat_pooled"))
   expect_equal(out$Items_der_Skala[1], c("skala1_item1,skala1_item2,skala1_item3"))
-  expect_equal(out$Items_der_Skala[2], c(""))
-  expect_equal(out$Items_der_Skala[3], paste(paste0("pv_", 1:5), collapse = ","))
+  expect_equal(out$Items_der_Skala[2], paste(paste0("pv_", 1:5), collapse = ","))
 })
+
+test_that("fake scale", {
+  out <- createScaleInfo(input_clean2)
+  expect_equal(nrow(out), 4)
+  expect_equal(out$Items_der_Skala[2], paste(paste0("skala_fake_item", 1:3), collapse = ","))
+})
+
 
 test_that("no scales, only imputed", {
   out <- createScaleInfo(input_pisa)
@@ -24,13 +28,13 @@ test_that("no scales, only imputed", {
 test_that("with input as list", {
   l1 <- list(pisa = input_pisa, other = input_clean)
   out <- createScaleInfo(l1)
-  expect_equal(nrow(out), 7)
-  expect_equal(out$Quelle, c(rep("pisa",3), rep("other",4)))
+  expect_equal(nrow(out), 6)
+  expect_equal(out$Quelle, c(rep("pisa", 3), rep("other", 3)))
 
   l2 <- list(other1 = input_clean, other2 = input_clean)
   out2 <- createScaleInfo(l2)
-  expect_equal(nrow(out2), 8)
-  expect_equal(out2$Quelle, rep(c("other1", "other2"), each=4))
+  expect_equal(nrow(out2), 6)
+  expect_equal(out2$Quelle, rep(c("other1", "other2"), each = 3))
 })
 
 test_that("imputed scale", {
