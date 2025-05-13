43c2c3d270c6c9777c229389a86c06fa16ba8af8
Benjamin <benj.becker@web.de>
2025-05-13 14:23:30 +0200

    toc_linebreak: Refactor code, fix non-breaking whitespace bug
    Substitute all non-breaking whitespaces in variable titles with common whitespaces. This avoids infinite recursion calls by toc_linebreak in case a title is long and contains no ordinary whitespaces. | Refactoring mainly aimed at readability and reducing computational repetitiveness.

--- R/toc_linebreak.R
+++ R/toc_linebreak.R
@@ -3,15 +3,30 @@ toc_linebreak <- function( var_title, bold , space_title, do.print=TRUE ){
   if(do.print){
     message("Check, ob Zeilenumbrueche ins Inhaltsverzeichnis muessen (rekursive Funktion).")
   }
+
+  #if(grepl("^Haben", var_title)) browser()
+
+  var_title_clean <- gsub("[\\p{Zs}]+", " ", var_title, perl = TRUE)
+  # clean non-breaking whitespaces (https://blog.tonytsai.name/blog/2017-12-04-detecting-non-breaking-space-in-r/)
+  # otherwise leads potentially to not infinite recursion as function can not find any whitespace in a title that is too long
+
+  splitted_strings_with_ws <-  unname(unlist(strsplit(var_title_clean , "(?<= )", perl = TRUE)))
+  splitted_strings <-  unname(unlist(strsplit(var_title_clean , " ", perl = TRUE)))
+  length_of_splitted_strings <- sapply(splitted_strings_with_ws, Latex.length, bold = bold, in.cm=FALSE)
+
   k <- 1
-  while(sum(sapply(1:k , function(v) Latex.length( unname(unlist(strsplit(var_title , " ")))[v] , bold=bold , FALSE) +  Latex.length( " " , bold=bold, FALSE)) ) < space_title){
+  while(sum(length_of_splitted_strings[1:k]) < space_title){
     k <- k+1
   }
-  l <- nchar(paste0(unname(unlist(strsplit(var_title , " ")))[1:(k-1)] , collapse=" "))
-  left_title <- paste0(unname(unlist(strsplit(var_title , " ")))[k:length(unname(unlist(strsplit(var_title , " "))))] , collapse=" ")
+
+  l <- nchar(paste0(splitted_strings[1:(k-1)] , collapse=" "))
+  left_title <- paste0(splitted_strings[k:length(splitted_strings)] , collapse=" ")
   if( Latex.length(left_title, bold=bold, FALSE) > space_title ){
-    return( paste0(paste0(unname(unlist(strsplit(var_title , " ")))[1:(k-1)] , collapse=" ") , "\\texorpdfstring{\\newline}{}" , toc_linebreak( left_title , bold , space_title , do.print=FALSE) ) )
+    pre_linebreak <- paste0(splitted_strings[1:(k-1)] , collapse=" ")
+    post_linebreak <- toc_linebreak( left_title , bold , space_title , do.print=FALSE)
+    return(paste0(pre_linebreak, "\\texorpdfstring{\\newline}{}" , post_linebreak ))
   } else {
-    return(paste0(paste0(unname(unlist(strsplit(var_title , " ")))[1:(k-1)] , collapse=" ") , "\\texorpdfstring{\\newline}{}" , left_title ) )
+    pre_linebreak <- paste0(splitted_strings[1:(k-1)] , collapse=" ")
+    return(paste0(pre_linebreak, "\\texorpdfstring{\\newline}{}" , left_title ) )
   }
 }
